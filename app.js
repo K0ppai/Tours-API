c70519023bdf48f127eddcd0a6c828de2f06cdea
koppai <psthu.koppai@gmail.com>
2023-12-30 16:05:18 +0630

    Create better structure folder

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import fs from 'fs';
+import morgan from 'morgan';
+import userRouter from './routes/userRoutes.js';
+import tourRouter from './routes/tourRoutes.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/tours', tourRouter);
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 2');
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+export default app;
