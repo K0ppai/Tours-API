d00867a539583e76c4b67db1d70b4b5f7d383c68
koppai <psthu.koppai@gmail.com>
2023-12-31 21:11:57 +0630

    Host static sites

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ import fs from 'fs';
 import morgan from 'morgan';
 import userRouter from './routes/userRoutes.js';
 import tourRouter from './routes/tourRoutes.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
