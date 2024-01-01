f900ac60062a29dc17423866b0fcd325071c5572
koppai <psthu.koppai@gmail.com>
2024-01-01 20:42:24 +0630

    Use env in app

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 import express from 'express';
-import fs from 'fs';
 import morgan from 'morgan';
 import userRouter from './routes/userRoutes.js';
 import tourRouter from './routes/tourRoutes.js';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import dotenv from 'dotenv';
+dotenv.config({ path: './config.env' });
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
-app.use(morgan('dev'));
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
