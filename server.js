25e5baaa74c916804520fad9ba8e29e69d5c87af
koppai <psthu.koppai@gmail.com>
2024-01-10 00:20:37 +0630

    Connect hosted db

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 import app from './app.js';
+import mongoose from 'mongoose';
 
 const port = process.env.PORT || 3000;
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB).then(() => {
+  console.log('DB connection successful');
+});
 
 app.listen(port, () => {
   console.log(`Server listening to port ${port}`);
