b5c0b8fd1133a784e356b770f83346fa9c463164
koppai <psthu.koppai@gmail.com>
2024-01-16 14:07:49 +0630

    Fix missing object keys AppError class

--- src/controllers/errorController.ts
+++ src/controllers/errorController.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
-import { TErrorHandler } from 'types/errorHandlerTypes';
+import { TErrorHandler } from 'types/errorHandlerType';
 
 const globalErrorHandler = (
   err: TErrorHandler,
@@ -7,6 +7,9 @@ const globalErrorHandler = (
   res: Response,
   _next: NextFunction
 ) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
