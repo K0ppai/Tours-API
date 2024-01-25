0d91e486d0ef39992570e529adf72c01485472e3
koppai <psthu.koppai@gmail.com>
2024-01-25 21:54:01 +0630

    Import data

--- src/dev-data/import-data.ts
+++ src/dev-data/import-data.ts
@@ -1,4 +1,6 @@
 import Tour from '../models/tourModel';
+import User from '../models/userModel';
+import Review from '../models/reviewModel';
 import mongoose from 'mongoose';
 import fs from 'fs';
 import dotenv from 'dotenv';
@@ -16,10 +18,18 @@ mongoose.connect(DB).then(() => {
 const tours: any = JSON.parse(
   fs.readFileSync(`${__dirname}/data/tours.json`).toString()
 );
+const users: any = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/users.json`).toString()
+);
+const reviews: any = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/reviews.json`).toString()
+);
 
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data successfully deleted');
     process.exit();
   } catch (error) {
@@ -30,14 +40,14 @@ const deleteData = async () => {
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data successfully imported');
     process.exit();
   } catch (error) {
     console.log(error);
   }
 };
-
-console.log(process.argv);
 
 if (process.argv[2] === '--import') {
   importData();
