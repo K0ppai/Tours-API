c127a03650461dbbf0e19f9a0cce275f5973d328
koppai <psthu.koppai@gmail.com>
2024-01-26 19:18:08 +0630

    Update types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,5 @@
 import { Request } from 'express';
+import { Document, Query, Schema } from 'mongoose';
 import { Model, Types } from 'mongoose';
 
 // Tour Types
@@ -31,7 +32,7 @@ export interface ITour {
   guides: IUser[];
 }
 
-export interface TourModelType extends Model<ITour> {
+export interface ITourQuery extends Query<ITour[], ITour> {
   startTime: number;
 }
 
@@ -61,12 +62,19 @@ export interface IUserMethods {
 }
 
 // Review Types
-export interface IReview {
+export interface IReview extends Document {
   review: string;
   rating: number;
   createdAt: Date;
-  tour: ITour;
-  user: IUser;
+  tour: Schema.Types.ObjectId;
+  user: Schema.Types.ObjectId;
+  constructor: {
+    calcAverageRating(tourId: Schema.Types.ObjectId): Promise<void>;
+  };
+}
+
+export interface IReviewQuery extends Query<IReview[], IReview> {
+  review: IReview;
 }
 
 // Types related to requests
